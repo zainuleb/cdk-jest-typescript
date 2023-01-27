f71232abc034abdc4d3eed05f92bf927ce1a7fb4
zainuleb <zainulebadd@gmail.com>
2023-01-27 05:08:03 +0500

    Initial commit

---
+++ test/cdk-jest.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkJest from '../lib/cdk-jest-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-jest-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkJest.CdkJestStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
